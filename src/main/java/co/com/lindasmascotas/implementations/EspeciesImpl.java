59be74a633a6e8fbe8d96e8a338d9ffc8fd96e8f
Veronica Velez <verovelez1706@gmail.com>
2018-03-17 14:36:35 -0500

    Implementacion de tablas maestras Cargos,Especies,Perfiles,Sexos,TiposDocumento,Vacunas.

---
+++ src/main/java/co/com/lindasmascotas/implementations/EspeciesImpl.java
@@ -0,0 +1,68 @@
+
+package co.com.lindasmascotas.implementations;
+
+import co.com.lindasmascotas.JPAcontrollers.EspeciesJpaController;
+import co.com.lindasmascotas.JPAcontrollers.exceptions.IllegalOrphanException;
+import co.com.lindasmascotas.JPAcontrollers.exceptions.NonexistentEntityException;
+import co.com.lindasmascotas.entities.Especies;
+import co.com.lindasmascotas.services.EspeciesSvc;
+import co.com.lindasmascotas.util.UPfactory;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class EspeciesImpl implements EspeciesSvc{
+
+    @Override
+    public List<Especies> listarEspecies() {
+        EspeciesJpaController ctrl = new EspeciesJpaController(UPfactory.getFACTORY());
+
+        return ctrl.findEspeciesEntities();
+    }
+
+    @Override
+    public List<Especies> crear(Especies e) {
+        EspeciesJpaController ctrl = new EspeciesJpaController(UPfactory.getFACTORY());
+
+        try {
+            ctrl.create(e);
+        } catch (Exception ex) {
+            Logger.getLogger(EspeciesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listarEspecies();
+    }
+
+    @Override
+    public List<Especies> editar(Especies e) {
+        EspeciesJpaController ctrl = new EspeciesJpaController(UPfactory.getFACTORY());
+        Especies especieActual = ctrl.findEspecies(e.getIdEspecie());
+
+        especieActual.setNombreEspecie(e.getNombreEspecie());
+
+        try {
+            ctrl.edit(especieActual);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(EspeciesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(EspeciesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listarEspecies();
+    }
+
+    @Override
+    public List<Especies> eliminar(Especies e) {
+        EspeciesJpaController ctrl = new EspeciesJpaController(UPfactory.getFACTORY());
+
+        try {
+            ctrl.destroy(e.getIdEspecie());
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(EspeciesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(EspeciesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return listarEspecies();
+    }
+    
+}
---
+++ src/main/java/co/com/lindasmascotas/implementations/PerfilesImpl.java
@@ -0,0 +1,68 @@
+
+package co.com.lindasmascotas.implementations;
+
+import co.com.lindasmascotas.JPAcontrollers.PerfilesJpaController;
+import co.com.lindasmascotas.JPAcontrollers.exceptions.IllegalOrphanException;
+import co.com.lindasmascotas.JPAcontrollers.exceptions.NonexistentEntityException;
+import co.com.lindasmascotas.entities.Perfiles;
+import co.com.lindasmascotas.services.PerfilesSvc;
+import co.com.lindasmascotas.util.UPfactory;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class PerfilesImpl implements PerfilesSvc {
+
+    @Override
+    public List<Perfiles> listarPerfiles() {
+        PerfilesJpaController ctrl = new PerfilesJpaController(UPfactory.getFACTORY());
+
+        return ctrl.findPerfilesEntities();
+    }
+
+    @Override
+    public List<Perfiles> crear(Perfiles p) {
+        PerfilesJpaController ctrl = new PerfilesJpaController(UPfactory.getFACTORY());
+
+        try {
+            ctrl.create(p);
+        } catch (Exception ex) {
+            Logger.getLogger(PerfilesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listarPerfiles();
+    }
+
+    @Override
+    public List<Perfiles> editar(Perfiles p) {
+        PerfilesJpaController ctrl = new PerfilesJpaController(UPfactory.getFACTORY());
+        Perfiles perfilActual = ctrl.findPerfiles(p.getIdPerfil());
+
+        perfilActual.setNombrePerfil(p.getNombrePerfil());
+
+        try {
+            ctrl.edit(perfilActual);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(PerfilesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(PerfilesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listarPerfiles();
+    }
+
+    @Override
+    public List<Perfiles> eliminar(Perfiles p) {
+        PerfilesJpaController ctrl = new PerfilesJpaController(UPfactory.getFACTORY());
+
+        try {
+            ctrl.destroy(p.getIdPerfil());
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(PerfilesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(PerfilesImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return listarPerfiles();
+    }
+    
+}
